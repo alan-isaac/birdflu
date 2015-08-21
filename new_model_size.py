2d09bf4a19cb51dcfab0d0f54582416e231b9d42
Alan Isaac <alan.isaac@gmail.com>
2015-08-21 15:21:38 -0400

    road stuff

---
+++ new_model_size.py
@@ -0,0 +1,16 @@
+"""
+We are going to take the original intersection data
+and scale up the world by a factor of 5.
+Note: don't change the road data,
+which connects intersections based on their integer ids.
+Comment: larger scale factor will exponentially slow setup.
+"""
+scale = 2  #scale factor
+with open("original_intersections.txt",'r') as fin, open("intersections.txt",'w') as fout:
+	result = [next(fin).strip()]
+	for line in fin:
+		x, y, name, itype = line.split()
+		#separate by *single* space
+		result.append("{} {} {} {}".format(scale*int(x), scale*int(y), name, itype))
+	fout.write("\n".join(result))
+
